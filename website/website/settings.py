d120920160bb7742bca1d835c51c93b935e813af
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-08-11 19:38:38 -0400

    Allowed all host to access the site

--- website/website/settings.py
+++ website/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0q3iviysu$81yn)gk!(-sqol8x6i5*nfiyxkj_$hs@&&5$fpxe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
