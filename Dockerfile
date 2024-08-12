ba356380e8e925f512db62ef6103c6a4c89e2d19
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-08-11 22:36:47 -0400

    Fixed bug with docker secret in django

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,13 @@ FROM python:3.10-slim
 WORKDIR /app
 
 # Copy the dependencies file to the working directory
-COPY ../requirements.txt .
+COPY requirements.txt .
 
 # Install any dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy the content of the local src directory to the working directory
-COPY . .
+COPY ./website .
 
 # Specify the command to run on container start
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- website/website/settings.py
+++ website/website/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = open(os.environ['SECRET_KEY'], "r").read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,16 +70,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'website.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'personal_website',
         'USER': 'django',
-        'PASSWORD': os.environ['DB_PASSWORD'],
+        'PASSWORD': open(os.environ['DB_PASSWORD'], "r").read(),
         'HOST': 'personal-website-db',
         'PORT': '5432',
     }
